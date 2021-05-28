a2e8135171868880314763adb0595de1ac27f061
Petrina <27221915+petrina-m@users.noreply.github.com>
2021-05-28 21:43:37 +0300

    -

--- lab13/src/app/LocaleExplore.java
+++ lab13/src/app/LocaleExplore.java
@@ -4,7 +4,9 @@ import com.DisplayLocales;
 import com.Info;
 import com.SetLocale;
 
+import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class LocaleExplore {
@@ -13,6 +15,10 @@ public class LocaleExplore {
         Locale locale2 = new Locale ("ro", "RO");
 
         while(true) {
+            String baseName = "res.Messages";
+            ResourceBundle messages =
+                    ResourceBundle.getBundle(baseName, locale);
+            System.out.println(messages.getString("prompt"));
             Scanner scanner = new Scanner(System.in);
             String inputString = scanner.nextLine();
             ExecuteCommand(inputString);
@@ -20,23 +26,35 @@ public class LocaleExplore {
 
     }
     static void ExecuteCommand(String command)
-    {
+    {  Locale locale=Locale.getDefault();
+        String baseName = "res.Messages";
+        ResourceBundle messages =
+                ResourceBundle.getBundle(baseName, locale);
+
         int displayIndex=command.indexOf("display");
         int setIndex=command.indexOf("set");
         int infoIndex=command.indexOf("info");
 
         if (displayIndex!=-1)
         {
+            System.out.println(messages.getString("locales"));
             DisplayLocales.run();
         }
-        if(setIndex!=-1)
+         else if(setIndex!=-1)
         {
             String[] words=command.split(" ");
             SetLocale.run(words[1]);
+            locale = Locale.getDefault();
+            System.out.println(MessageFormat.format(messages.getString("locale.set"), locale.getLanguage()));
         }
-        if(infoIndex!=-1)
+        else if(infoIndex!=-1)
         {
+            System.out.println(MessageFormat.format(messages.getString("info"), locale.getLanguage()));
             Info.run();
+        }
+        else
+        {
+            System.out.println(messages.getString("invalid"));
         }
     }
 
